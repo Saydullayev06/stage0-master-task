c049f353bc0d238c154e365c5cc21ee2bd40212e
Saydullayev06 <125748237+Saydullayev06@users.noreply.github.com>
2023-03-04 23:34:01 -0700

    Update DaysInMonth.java

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,17 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
+        if((month==2) && ((year%4==0) || ((year%100==0)&&(year%400==0))))
+            System.out.println("Number of days is 29");
 
+        else if(month==2)
+            System.out.println("Number of days is 28");
+
+        else if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12)
+            System.out.println("Number of days is 31");
+
+        else
+            System.out.println("Number of days is 30");
     }
 
 }
